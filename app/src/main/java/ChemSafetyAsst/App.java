44f6c2577e296a91c7df58390b3563da760393ba
sarah <skiyoko14@gmail.com>
2022-08-09 23:20:07 -0700

    commit to redo action

--- app/src/main/java/ChemSafetyAsst/App.java
+++ app/src/main/java/ChemSafetyAsst/App.java
@@ -167,6 +167,7 @@ public class App {
     public static void main(String[] args) {
         // Does this make the app??? check later
         // System.out.println(new App().getGreeting());
+        // port for deployment
         port(80);
         // Setup; need one of each of these 
         enableCORS("*", "GET", "*");
