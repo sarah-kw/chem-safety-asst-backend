2e3a88e8424b5da11f45dbb4ab480b24b80825aa
sarah <skiyoko14@gmail.com>
2022-08-09 23:11:01 -0700

    change port back to 80

--- app/src/main/java/ChemSafetyAsst/App.java
+++ app/src/main/java/ChemSafetyAsst/App.java
@@ -167,7 +167,7 @@ public class App {
     public static void main(String[] args) {
         // Does this make the app??? check later
         // System.out.println(new App().getGreeting());
-        port(443);
+        port(80);
         // Setup; need one of each of these 
         enableCORS("*", "GET", "*");
         HttpClient client = HttpClient.newBuilder().build();
